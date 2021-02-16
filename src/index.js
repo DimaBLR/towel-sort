bd7b8cb9605e0841d02a7eb890ca4cccb1dcbfbc
DimaBLR <76612985+DimaBLR@users.noreply.github.com>
2021-02-16 20:09:07 +0300

    fix: fixed code and add error handler

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@
 module.exports = function towelSort (matrix) {
     let arr = [];
 
+    if (arguments.length == 0) {
+        return arr;
+    }
+    
+    for (let n = 0; n < matrix.length; n++) {
+        if (n % 2 !== 0) {
+            matrix[n] = matrix[n].sort((a, b) => b - a);
+        }
+    }
+
     for (let i = 0; i < matrix.length; i++) {
         arr = arr.concat(matrix[i]);
-    }    
+    }
 
-    return arr.sort((a, b) => a - b);
+    return arr;
 }
